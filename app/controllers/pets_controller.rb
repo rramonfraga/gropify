3b648a45954e52206c5f03a6f0bda8f70fee1cf1
Rafael Ramon Fraga <rafael_ramon_fraga@yahoo.es>
2016-01-13 10:07:55 +0100

    Add GET index to the controller and the test in pets_controller_spec

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,4 @@
 class PetsController < ApplicationController
-  before_action :authenticate_user!
 
   def index
     @pets = Pet.last_created_pets_desc
--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe PetsController, type: :controller do
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
 
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "loads all of the pets into @pets" do
+      pet1 = Pet.create name: "Luigi", kind: "Dog", age: 3
+      pet2 = Pet.create name: "Mario", kind: "Cat", age: 2
+      get :index
+
+      expect(assigns(:pets)).to match_array([pet1, pet2])
+    end
+  end
 end
